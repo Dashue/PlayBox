3472cf7dfa671cb0a87b463f4ac4a49e6b3af396
Johan Nilsson <jonh04@gmail.com>
2016-10-26 19:21:44 +0100

    Create angularjs2 skeleton project

---
+++ Frontends/angularjs2/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { TodoComponent } from './todo.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [TodoComponent],
+    bootstrap: [TodoComponent]
+})
+export class AppModule { }
---
+++ Frontends/angularjs2/app/todo.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'todo',
+  template: '<h1>Todo</h1>'
+})
+export class TodoComponent { }
